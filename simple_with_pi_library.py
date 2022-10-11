fd0741682bcac1a063e97617a58d3a94a07b0338
Richard Kirby <richard.james.kirby@gmail.com>
2022-10-11 17:51:23 +0100

    Minor updates - change timer and output, etc.

--- simple_with_pi_library.py
+++ simple_with_pi_library.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 import HX711 as HX
+
 
 # print(hx.__version__)
 
@@ -6,7 +8,7 @@ print("start")
 # create a SimpleHX711 object using GPIO pin 2 as the data pin,
 # GPIO pin 3 as the clock pin, -370 as the reference unit, and
 # -367471 as the offset
-with HX.SimpleHX711(15, 14, int(-370 / 1.244), -367471) as hx:
+with HX.SimpleHX711(14, 15, int(-370 / 1.244), -367471) as hx:
   print("with")
   # set the scale to output weights in ounces
   # hx.setUnit(Mass.Unit.)
@@ -18,9 +20,9 @@ with HX.SimpleHX711(15, 14, int(-370 / 1.244), -367471) as hx:
 
   # constantly output weights using the median of 35 samples
   while True:
-    weight_list.append(hx.weight(5)) #eg. 1.08 oz
-    if len(weight_list) == 50:
-      print(weight_list)
+    weight_list.append(hx.weight(2)) #eg. 1.08 oz
+    if len(weight_list) == 10:
+      print(datetime.now(), weight_list)
       weight_list=[]
 
 
