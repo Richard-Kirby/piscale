805048f35a49da2c004795ed6f3bc04080ef5bb1
Richard Kirby <richard.james.kirby@gmail.com>
2022-10-22 17:17:03 +0100

    Working version with a simple UI.

--- scale_gui.py
+++ scale_gui.py
@@ -9,6 +9,16 @@ import HX711 as HX
 class App(tk.Frame):
     def __init__(self, master=None):
 
+        tk.Frame.__init__(self, master)
+        self.master = master
+
+        style = ttk.Style()
+        print(style.theme_names())
+        style.theme_use("alt")
+        style.configure('Treeview', rowheight=20)
+        style.map("Treeview")
+
+
         # Connect to the scale and zero it out.
         # create a SimpleHX711 object using GPIO pin 14 as the data pin,
         # GPIO pin 15 as the clock pin, -370 as the reference unit, and
@@ -16,40 +26,95 @@ class App(tk.Frame):
         self.hx = HX.SimpleHX711(14, 15, int(-370 / 1.244), -367471)
         self.hx.zero()
 
-        tk.Frame.__init__(self, master)
+        # Update the weight, which will happen regularly after this call.
+        self.weight_disp = tk.Label(text="", fg="Red", font=("Helvetica", 30))
+        self.update_weight()
 
-        # configure the grid
+
+        # Configure the grid for all the widgets.
         root.columnconfigure(0, weight=1)
         root.columnconfigure(1, weight=1)
         root.columnconfigure(2, weight=1)
-
-        #root.columnconfigure(3, weight=1)
-
-        self.master = master
-        self.time_label = tk.Label(text="", fg="Blue", font=("Helvetica", 18))
-        #self.label.place(x=50,y=80)
-        self.weight_disp = tk.Label(text="", fg="Red", font=("Helvetica", 30))
-
-        #self.weight_disp.place(x=50, y=100)
-        # self.weight_disp.pack()
-        self.update_weight()
-        # create a button
-
-        self.zero_btn = tk.Button(self.master, text="Zero", command=self.zero)
-        self.exit_btn = tk.Button(self.master, text="Exit", command=self.exit)
-        self.add_to_meal_btn = tk.Button(self.master, text="Add To Meal", command=self.add_to_meal)
+        root.columnconfigure(3, weight=1)
+        root.columnconfigure(4, weight=1)
+        root.columnconfigure(5, weight=1)
+        root.columnconfigure(6, weight=1)
 
         # Connection to database of food.
         self.db_con = sq.connect('food_data.db')
 
+        self.time_label = tk.Label(text="", fg="Blue", font=("Helvetica", 18))
+
+        self.zero_btn = tk.Button(self.master, text="Zero", command=self.zero)
+        self.exit_btn = tk.Button(self.master, text="Exit", command=self.exit)
+        self.add_to_meal_btn = tk.Button(self.master, text="->", command=self.add_to_meal)
+
+        # Create the Food Data Tree
+        self.food_data_frame = tk.Frame(master)
+        self.create_food_data_tree(self.food_data_frame)
+
+        self.meal_frame = tk.Frame(master)
+        # Create the Food Data Tree
+        self.create_meal_tree(self.meal_frame)
+
+        # Intialise the mawl to 0 caories.
+        self.meal_total_calories = 0
+        self.meal_kcal_display = tk.Label(text="0", fg="Red", font=("Helvetica", 15))
+
+        self.favorite_radio_sel = tk.IntVar()
+
+        all_radio = tk.Radiobutton(self.master,
+                       text="All",
+                       variable=self.favorite_radio_sel,
+                       command=self.radio_sel,
+                       value=0)
+
+        fave_radio = tk.Radiobutton(self.master,
+                       text="Fav",
+                       variable=self.favorite_radio_sel,
+                       command=self.radio_sel,
+                       value=1)
+
+        # Populate all the data from the Database of information
+        self.populate_food_data()
+
+        # Button to Remove something from the meal.
+        self.remove_from_meal_btn = tk.Button(self.master, text="<-", command=self.remove_from_meal)
+
+        # Buttons to toggle whether a food is a favourite or not, to enable filtering.
+        self.toggle_favourite_btn = tk.Button(self.master, text="Fav 0/1", command=self.toggle_favourite)
+
+        # Widget placements
+        self.time_label.grid(column=0, row=0, columnspan=3)
+        self.exit_btn.grid(column=6, row=0)
+
+        self.weight_disp.grid(column=0, row=1, columnspan=2)
+        self.zero_btn.grid(column=2, row=1)
+
+        self.food_data_frame.grid(column=0, row=2, columnspan=3, rowspan=15)
+
+        self.toggle_favourite_btn.grid(column=3, row=2)
+        fave_radio.grid(column=3, row=3)
+        all_radio.grid(column=3, row=4)
+
+        self.add_to_meal_btn.grid(column=3, row=6)
+        self.remove_from_meal_btn.grid(column=3, row=7)
+
+        self.meal_frame.grid(column=4, row=2, columnspan=3, rowspan=15)
+
+        self.meal_kcal_display.grid(column=6, row=19)
+
+    # Creates the Food Data Tree for selecting food. Puts it into a frame.
+    def create_food_data_tree(self, food_data_frame):
+
         # Setting Style
-        style = ttk.Style()
-        style.theme_use("default")
-        style.configure('Treeview', rowheight=20)
-        style.map("Treeview")
+
+        # Set up frame to have 2 columns
+        food_data_frame.columnconfigure(0, weight=4)
+        food_data_frame.columnconfigure(1, weight=1)
 
         # List of food and their characteristics.
-        self.food_tree_view = ttk.Treeview(self.master, columns=('db_id', 'FoodName', 'kCal', 'Fave'),
+        self.food_tree_view = ttk.Treeview(food_data_frame, columns=('db_id', 'FoodName', 'kCal', 'Fave'),
                                            show='headings', height=12)
 
         self.food_tree_view["displaycolumns"]=('FoodName', 'kCal', 'Fave')
@@ -59,19 +124,23 @@ class App(tk.Frame):
         self.food_tree_view.heading('FoodName', text="Food Name")
         self.food_tree_view.heading('kCal', text="kCal/100g")
         self.food_tree_view.heading('Fave', text="Fave")
-        self.food_tree_view.grid(column=0, row=2)
+        self.food_tree_view.grid(column=0, row=0)
 
-        sb = ttk.Scrollbar(self.master, orient=tk.VERTICAL) # was frame.
-        sb.grid(column=1, row=2)
+        sb = ttk.Scrollbar(food_data_frame, orient=tk.VERTICAL)
+        sb.grid(column=1, row=0, sticky='ns')
 
         self.food_tree_view.config(yscrollcommand=sb.set)
         sb.config(command=self.food_tree_view.yview)
 
-        # Populate all the data from the Database of information
-        self.populate_food_data()
+    # Creates the meal Tree for showing the meal. Puts it into a frame.
+    def create_meal_tree(self, meal_frame):
+
+        # Set up frame to have 2 columns
+        meal_frame.columnconfigure(0, weight=4)
+        meal_frame.columnconfigure(1, weight=1)
 
         # Create the meal TreeView, which tracks the meal
-        self.meal_tree_view = ttk.Treeview(self.master, columns=('FoodName', 'Weight', 'kCal'), show='headings', height=12)
+        self.meal_tree_view = ttk.Treeview(meal_frame, columns=('FoodName', 'Weight', 'kCal'), show='headings', height=12)
         self.meal_tree_view.column('FoodName', anchor=tk.CENTER, width=100)
         self.meal_tree_view.column('Weight', anchor=tk.CENTER, width=80)
         self.meal_tree_view.column('kCal', anchor=tk.CENTER, width=80)
@@ -80,48 +149,24 @@ class App(tk.Frame):
         self.meal_tree_view.heading('kCal', text="kCal")
         self.meal_tree_view.heading('Weight', text="grams")
 
-        # Intialise the mawl to 0 caories.
-        self.meal_total_calories = 0
-        self.meal_kcal_display = tk.Label(text="0", fg="Red", font=("Helvetica", 15))
+        self.meal_tree_view.grid(column=0, row=0)
+        sb = ttk.Scrollbar(meal_frame, orient=tk.VERTICAL)
+        sb.grid(column=1, row=0, sticky='ns')
 
-        self.favorite_radio_sel = tk.IntVar()
-
-        fave_radio = tk.Radiobutton(self.master,
-                       text="Favorites",
-                       variable=self.favorite_radio_sel,
-                       command=self.radio_sel,
-                       value=1)
-
-        all_radio = tk.Radiobutton(self.master,
-                       text="All",
-                       variable=self.favorite_radio_sel,
-                       command=self.radio_sel,
-                       value=2)
-
-        # Button to Remove something from the meal.
-        self.remove_from_meal_btn = tk.Button(self.master, text="Remove From Meal", command=self.remove_from_meal)
-
-        # Buttons to toggle whether a food is a favourite or not, to enable filtering.
-        self.toggle_favourite_btn = tk.Button(self.master, text="Favourite Toggle", command=self.toggle_favourite)
-
-        self.time_label.grid(column=0, row=0)
-        self.weight_disp.grid(column=0, row=1)
-        self.zero_btn.grid(column=1, row=0)
-        self.exit_btn.grid(column=1, row=1)
-        self.add_to_meal_btn.grid(column=2, row=1)
-        self.remove_from_meal_btn.grid(column=3, row=1)
-        self.meal_tree_view.grid(column=2, row=2, columnspan=2)
-        self.meal_kcal_display.grid(column=3, row=3)
-        fave_radio.grid(column=0, row=3)
-        all_radio.grid(column=0, row=4)
-        self.toggle_favourite_btn.grid(column=1, row=3)
+        self.meal_tree_view.config(yscrollcommand=sb.set)
+        sb.config(command=self.meal_tree_view.yview)
 
     # Grab the data from the Database and add it to the TreeView table.
     def populate_food_data(self):
         self.food_tree_view.delete(*self.food_tree_view.get_children())
 
         with self.db_con:
-            food_data = self.db_con.execute("SELECT id, FoodCode, FoodName, KCALS, Favourite FROM FoodData")
+            print(self.favorite_radio_sel.get())
+            if self.favorite_radio_sel.get() == 0:
+                food_data = self.db_con.execute("SELECT id, FoodCode, FoodName, KCALS, Favourite FROM FoodData")
+            else:
+                food_data = self.db_con.execute("SELECT id, FoodCode, FoodName, KCALS, Favourite FROM FoodData"
+                                                " WHERE Favourite=1")
 
         self.food_tree_view.tag_configure('odd', font=("default",12), background='light grey')
         self.food_tree_view.tag_configure('even', font=("default",12))
@@ -153,7 +198,7 @@ class App(tk.Frame):
 
     def update_weight(self):
         # Get the current weight on the scale
-        weight_str = str(self.hx.weight(2))
+        weight_str = str(self.hx.weight(10))
         # print(weight_str)
         self.weight = float(weight_str[:-2])
 
@@ -164,7 +209,7 @@ class App(tk.Frame):
 
         #print(weight)
         self.weight_disp.configure(text = weight_display)
-        self.after(500, self.update_weight)
+        self.after(1000, self.update_weight)
 
     def update_meal_calories(self):
         self.meal_kcal_display.configure(text = (f"{self.meal_total_calories:.0f} kCal"))
@@ -174,7 +219,7 @@ class App(tk.Frame):
         selected = self.food_tree_view.selection()
         chosenfood = self.food_tree_view.item(selected[0])
         print(chosenfood)
-        food_name, calories_per_100 = chosenfood["values"]
+        id, food_name, calories_per_100, fave = chosenfood["values"]
         print(self.weight)
         food_weight = self.weight
         print(food_name, calories_per_100)
@@ -210,13 +255,14 @@ class App(tk.Frame):
 
     def radio_sel(self):
         print(str(self.favorite_radio_sel.get()))
+        self.populate_food_data()
 
     def toggle_favourite(self):
         selected = self.food_tree_view.selection()
 
         for sel_item in selected:
             print(sel_item)
-            id = self.food_tree_view.index(sel_item)
+            id = self.food_tree_view.item(selected[0])["values"][0]
 
             if self.food_tree_view.item(selected[0])["values"][3] == 0:
                 Favourite = 1
@@ -228,7 +274,8 @@ class App(tk.Frame):
 
             # This doesn't seem to be working. 
             with self.db_con:
-                print(self.db_con.execute("UPDATE FoodData SET Favourite = ? where id= ?", [Favourite , id]))
+                print(self.db_con.execute("UPDATE FoodData SET Favourite = ? where id= ?", [Favourite , id]),
+                      Favourite, id)
 
         time.sleep(1)
         self.populate_food_data()
